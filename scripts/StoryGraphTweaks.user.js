e6771a9bb77822e45fe32dd1457689abe62fae03
Jordy3D <fchetar@gmail.com>
2025-01-31 00:34:42 +1100

    Update StoryGraphTweaks to v0.0.4

--- scripts/StoryGraphTweaks.user.js
+++ scripts/StoryGraphTweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         StoryGraph Tweaks
 // @namespace    Bane
-// @version      0.0.3
+// @version      0.0.4
 // @description  Tweaks and changes to The StoryGraph
 // @author       Bane
 // @match        https://app.thestorygraph.com/*
@@ -19,14 +19,15 @@
 // - Add a button to copy book ID (done)
 // - Add a button to edit book (done)
 // - Add a button to mark as read today 
-// - Add a button to copy author ID
-// - Add a button to copy series ID
+// - Add a button to copy author ID (done)
+// - Add a button to copy series ID (done)
 
 // ==ChangeLog==
 // 0.0.1    - Initial version
 //              - Added buttons to copy book ID and jump to edit page
 // 0.0.2    - Added loop to check for new book result divs (on both infinite scroll and page load)
 // 0.0.3    - Code cleanup and refactoring
+// 0.0.4    - Added buttons to copy author and series IDs
 // ==/ChangeLog==
 
 //=====================================================================================================//
@@ -35,26 +36,98 @@
 
 function main() {
     addCustomCSS();
+    handleBookResults();
 
-    var bookResultDivs = findBookResultDivs();
-    bookResultDivs.forEach(bookResultDiv => {
-        if (bookResultDiv.hasAttribute("bane-tweaks")) return;
+    const path = window.location.pathname;
+    if (path.includes('/authors/'))
+        handleAuthorSection();
 
-        var bookDataId = findBookDataId(bookResultDiv);
-        var actionMenu = findActionMenu(bookResultDiv);
-        
-        var customActionContainer = addCustomActionContainer(actionMenu);
-        addEditButton(customActionContainer, bookDataId);
-        addCopyIDButton(customActionContainer, bookDataId);
+    if (path.includes('/series/'))
+        handleSeriesSection();
 
-        bookResultDiv.setAttribute("bane-tweaks", "");
-    });
 }
 
 main();
 setInterval(main, 1000);
 
-// Functions
+function handleBookResults() {
+    document.querySelectorAll(".book-pane[data-book-id]").forEach(bookResultDiv => {
+        if (bookResultDiv.hasAttribute("bane-tweaks")) return;
+
+        const bookDataId = bookResultDiv.getAttribute("data-book-id");
+        const actionMenu = bookResultDiv.querySelector("div.action-menu");
+
+        addCustomActions(actionMenu, bookDataId);
+        bookResultDiv.setAttribute("bane-tweaks", "");
+    });
+}
+
+function addCustomActions(actionMenu, bookDataId) {
+    const container = document.createElement("div");
+    container.classList.add("bane-action-menu");
+
+    // Add Edit button
+    const editButton = document.createElement("a");
+    editButton.innerText = "Edit";
+    editButton.classList.add("bane-edit-button", "bane-action-button");
+    editButton.href = `https://app.thestorygraph.com/books/${bookDataId}/edit`;
+    editButton.target = "_blank";
+
+    // Add Copy ID button
+    const copyIDButton = document.createElement("button");
+    copyIDButton.innerText = "Copy ID";
+    copyIDButton.classList.add("bane-edit-button", "bane-action-button");
+    copyIDButton.onclick = () => navigator.clipboard.writeText(bookDataId);
+
+    container.append(editButton, copyIDButton);
+    actionMenu.appendChild(container);
+}
+
+function handleAuthorSection() {
+    const barcodeModal = document.querySelector('#barcode-scanner-modal');
+    if (!barcodeModal) return;
+
+    const authorSection = barcodeModal.nextElementSibling;
+    if (!authorSection || authorSection.hasAttribute("bane-author-tweaks")) return;
+
+    const authorId = window.location.pathname.split('/').filter(Boolean)[1];
+    if (!authorId) return;
+
+    const menu = document.createElement('div');
+    menu.classList.add('bane-header-menu');
+
+    const copyIDButton = document.createElement("button");
+    copyIDButton.innerText = "Copy Author ID";
+    copyIDButton.classList.add("bane-header-button");
+    copyIDButton.onclick = () => navigator.clipboard.writeText(authorId);
+
+    menu.appendChild(copyIDButton);
+    authorSection.appendChild(menu);
+    authorSection.setAttribute("bane-author-tweaks", "");
+}
+
+function handleSeriesSection() {
+    const barcodeModal = document.querySelector('#barcode-scanner-modal');
+    if (!barcodeModal) return;
+
+    const seriesSection = barcodeModal.nextElementSibling;
+    if (!seriesSection || seriesSection.hasAttribute("bane-series-tweaks")) return;
+
+    const seriesId = window.location.pathname.split('/').filter(Boolean)[1];
+    if (!seriesId) return;
+
+    const menu = document.createElement('div');
+    menu.classList.add('bane-header-menu');
+
+    const copyIDButton = document.createElement("button");
+    copyIDButton.innerText = "Copy Series ID";
+    copyIDButton.classList.add("bane-header-button");
+    copyIDButton.onclick = () => navigator.clipboard.writeText(seriesId);
+
+    menu.appendChild(copyIDButton);
+    seriesSection.appendChild(menu);
+    seriesSection.setAttribute("bane-series-tweaks", "");
+}
 
 function addCustomCSS() {
     // check if the custom CSS has already been added
@@ -65,6 +138,7 @@ function addCustomCSS() {
             --darkGrey: rgb(229 229 229);
             --darkerGrey: rgb(102 102 102);
             --darkestGrey: rgb(51 51 51);
+            --highlight: rgb(56 190 201);
         }
 
         .bane-action-menu {
@@ -92,51 +166,54 @@ function addCustomCSS() {
         .bane-action-button:hover {
             background-color: var(--darkerGrey);
         }
+
+        [bane-author-tweaks],
+        [bane-series-tweaks] {
+            display: flex;
+            flex-direction: row;
+            gap: 10px;
+            margin-bottom: 10px;
+
+            > :first-child {
+                flex: 1;
+            }
+
+            .secondary-btn {
+                margin-top: 0;
+            }
+        }
+
+        .bane-header-menu {
+            display: flex;
+            flex-direction: column;
+            gap: 8px;
+
+            padding: 10px;
+            border-left: 1px solid var(--darkerGrey);
+        }
+
+        .bane-header-button {
+            cursor: pointer;
+            font-size: .75rem;
+            font-weight: 500;
+            line-height: 1rem;
+            background-color: var(--darkerGrey);
+
+            border-color: transparent;
+            border-radius: .5rem;
+            border-width: 1px;
+
+            text-align: center;
+            padding: 8px 16px;
+
+            &:hover {
+                background-color: var(--highlight);
+                color: var(--darkestGrey);
+            }
+        }
     `;
     var styleElement = document.createElement("style");
     styleElement.innerHTML = customCSS;
     styleElement.id = "bane-custom-css";
     document.head.appendChild(styleElement);
-}
-
-function findBookResultDivs() {
-    // find every div that is a proper book result
-    return document.querySelectorAll(".book-pane[data-book-id]");
-}
-
-function findBookDataId(bookResultDiv) {
-    // find the id of a book result div
-    return bookResultDiv.getAttribute("data-book-id");
-}
-
-function findActionMenu(bookResultDiv) {
-    // find the action menu of a book result div
-    return bookResultDiv.querySelector("div.action-menu");
-}
-
-function addCustomActionContainer(actionMenu) {
-    // add a custom action container to the action menu
-    var customActionContainer = document.createElement("div");
-    customActionContainer.classList.add("bane-action-menu");
-    actionMenu.appendChild(customActionContainer);
-    return customActionContainer;
-}
-
-function addEditButton(customActionContainer, bookDataId) {
-    // add an edit button to the custom action container
-    var editButton = document.createElement("a");
-    editButton.innerText = "Edit";
-    editButton.classList.add("bane-edit-button", "bane-action-button");
-    editButton.href = `https://app.thestorygraph.com/books/${bookDataId}/edit`;
-    editButton.target = "_blank";
-    customActionContainer.appendChild(editButton);
-}
-
-function addCopyIDButton(customActionContainer, bookDataId) {
-    // add a copy ID button to the custom action container
-    var copyIDButton = document.createElement("button");
-    copyIDButton.innerText = "Copy ID";
-    copyIDButton.classList.add("bane-edit-button", "bane-action-button");
-    copyIDButton.onclick = () => navigator.clipboard.writeText(bookDataId);
-    customActionContainer.appendChild(copyIDButton);
 }
